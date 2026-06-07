c671ef253b422cbbf49598915e8972566453e3d5
Min Naing Oo <minnaingoo792@gmail.com>
2026-06-07 19:46:28 +0630

    rooms

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ toggle.onclick = () =>{
     navmenu.classList.toggle('hidden');
     
 }
+var slider = tns({
+            container: '.my-slider',
+            items: 1,
+            responsive: {
+                640: {
+                    edgePadding: 20,
+                    gutter: 20,
+                    items: 2
+                },
+                700: {
+                    gutter: 30
+                },
+                900: {
+                    items: 3
+                }
+            },
+            mouseDrag: true,
+            autoplay: true,
+            controls: false,
+            autoplayButtonOutput: false,
+        });
+       
